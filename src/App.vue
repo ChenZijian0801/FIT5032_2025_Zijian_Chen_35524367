539c7eeadde94fd9f0a1a20efb707757887de9fd
ChenZijian0801 <zche0348@student.monash.edu>
2025-07-08 13:28:19 +0800

    Update_version2

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import JSON from './components/JSON.vue'; 
+import Form from "./components/Form.vue";
 </script>
 
 <template>
-  <div id="app">
-    <JSON /> 
-  </div>
+ 
+   
+  <Form/>
 </template>
 
 <style>
